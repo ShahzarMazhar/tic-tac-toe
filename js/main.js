dc17bf4ad9474b5464233c90c1b868c248610129
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-05-20 01:40:45 +0530

    Umbeatable AI added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-// const GameBoard = () => {
-//     const gameBoard = [];
-// }
-// Players () > player {}
-// Game {}
 
+
+//form Manipulation and saving players
 const Form = (() => {
     const welcome_model = document.querySelector('.welcome-model');
 
+    //player 1
     const p1 = (() => { 
         const p1 = welcome_model.querySelector('.p1');
         const ai_check = p1.querySelector('input[type=checkbox]');
@@ -17,6 +15,7 @@ const Form = (() => {
         return p1;
         })();
 
+    //player 2
     const p2 = (() => {
         const p2 = p1.cloneNode(true);
         const ai_check = p2.querySelector('input[type=checkbox]');
@@ -32,9 +31,11 @@ const Form = (() => {
         return p2;
     })();
 
+    //modify form for AI if (AI checked)
     const p1_bot = convertForm(p1);
     const p2_bot = convertForm(p2);
 
+    //AI checked event
     function change_AI(e){
         const checked   = e.target.checked;
         const id        = e.target.getAttribute('data-id')
@@ -47,12 +48,14 @@ const Form = (() => {
             }
     }
 
+    //form modification function for AI
     function convertForm(playerForm){
         const userIcon = playerForm.querySelector('h2 span');
         const div = playerForm.querySelector('.input-AI');
         const difficulty = playerForm.querySelector('.difficulty');
         difficulty.remove();
     
+        //closer to keep the nodes saved even if removed from DOM
         const showHide = show => {
             if(show){
                 div.appendChild(difficulty);
@@ -65,6 +68,7 @@ const Form = (() => {
         return showHide;
     }
 
+    //Save Player in Player Object
     function savePlayer(e){
         e.preventDefault();
 
@@ -79,11 +83,16 @@ const Form = (() => {
         saved();
     }
 
+
+    //after save player 1, open form for player 2
+    //after save player 2, setup game up
     function saved(){
         if(Players.getPlayer(1) != undefined) {
             welcome_model.closest('.welcome').remove();
-            GameBoard.setup();
-            Game.setup();
+            setTimeout(() => {
+                GameBoard.setup();
+                Game.setup();
+            }, 0);
             return
         };
         welcome_model.append(p2);
@@ -103,6 +112,7 @@ const Players = (() => {
     let id = 1;
     const player = [];
 
+    //player factory
     const setPlayer = (name, ai, difficulty) => {
         if(player.length > 1){
             alert('something went wrong');
@@ -120,7 +130,6 @@ const Players = (() => {
 
 const GameBoard = (() => {
     const gameBoard = [];
-    let winner;
     const winningCombination = [
         [0, 1, 2],
         [3, 4, 5],
@@ -134,6 +143,7 @@ const GameBoard = (() => {
         [2, 4, 6],
     ]
 
+    //make easy document.create (including bulk create)
     const createElements = (...elementName) => {
         const result = [];
         elementName.forEach(e => result.push(document.createElement(e)));
@@ -141,39 +151,30 @@ const GameBoard = (() => {
         return result;
     }
 
+    //make easy add Attribute (including bulk)
     const addAttributes = (qualifiedName, ...pairsOfNodeClass) => {
         pairsOfNodeClass.forEach(e => {
             e[0].setAttribute(qualifiedName, e[1]);
         })
     }
 
-    const setWinner = symbol => winner = symbol;
-
-
-    const findWinner = () => {
-        const gB = gameBoard;
-        let winnerSymbol, winnerCombination;
-        let draw = false;
+    //check winner and return true if won with winning combination
+    const checkWinner = (player, board) => {
+        const gB = board || gameBoard;
+        let result = false;
+        let winnerCombination;
         winningCombination.forEach(wC => {
             
             if(
-                (gB[wC[0]] != undefined) && 
-                (gB[wC[0]] == gB[wC[1]]) && 
-                (gB[wC[1]] == gB[wC[2]]) ){
-                    
-                    winnerSymbol = gB[wC[0]];
+                (gB[wC[0]] == player) && 
+                (gB[wC[1]] == player) && 
+                (gB[wC[2]] == player) ){
+                    result = true;
                     winnerCombination = wC;
-                    setWinner(winnerSymbol);
-                };
+                }
                 
             })
-            
-                if(!gB.includes(undefined) && gB.length == 9 && winnerSymbol == undefined){
-                    draw = true;
-                    // return;
-                }
-        return {winnerSymbol, winnerCombination, draw};
-    
+    return {result, winnerCombination};
     }
 
     const setup = () => {
@@ -225,27 +226,21 @@ const GameBoard = (() => {
         gameBoard[index] = symbol;
     }
 
-    const getWinner = () => {
-        return winner
-    }; 
+    const emptySlots = () => ArtificialIntelligence.emptySlots(gameBoard);
 
-    const emptySlots = () => {
-        const result = [];
-        for (let i = 0; i < 9; i++){
-            if(!gameBoard[i]){
-                result.push(i);
-            }
-        }
-        return result
-    };    
+    const get_board = (() => {
+        return gameBoard
+    })();
 
-    return{setup, update_gameBoard, getWinner, findWinner, emptySlots}
+    return{setup, update_gameBoard, emptySlots, checkWinner, get_board}
 })();
 
 const Game = (() => {
     let activePlayer;
     const sleep = ms => new Promise(r => setTimeout(r, ms));
+    let winnerAnnounced = false;
 
+    //render markers to the UI
     const render = (element) => {
         const icon = document.createElement('span');
         const symbol = (activePlayer == 'X') ? 'close' : 'circle-outline';
@@ -253,6 +248,18 @@ const Game = (() => {
         element.appendChild(icon);
     }
 
+    const updateWinSlots = (wC) => {
+
+        let t = 0 ;
+        wC.forEach(i => {
+            t += 500;
+            sleep(t).then(() => {
+                document.querySelector(`.box-${i}`).classList.add('selected');
+            })
+        })
+    }
+
+    //show result upon call
     const showResult = result => {
         const resultDisplay = document.querySelector('.result');
         const resultDisplayModel = resultDisplay.querySelector('.result-model');
@@ -260,25 +267,37 @@ const Game = (() => {
         const button = resultDisplay.querySelector('button');
         const str = (result == "Draw!") ? "Draw!" : `${result.toUpperCase()} Won!` ;
         msg.textContent = str;
-        resultDisplayModel.classList.remove('translateY130');
+        document.querySelector('.result').style.zIndex = '1'
+        setTimeout(() => {
+            resultDisplayModel.classList.remove('translateY130');
+        }, 0);
         button.addEventListener('click', replay)
     }
 
 
+    //call showResult() if won or draw else call changePlayer()
     const announceWinner = () => {
-        const winner = GameBoard.findWinner();
-        if(winner.draw) {
+        const winner = GameBoard.checkWinner(activePlayer);
+        const availSpots = ArtificialIntelligence.emptySlots(GameBoard.get_board);
+
+        //Won
+        if(winner.result){
+            const playerName = (activePlayer == 'X') ? Players.getPlayer(0).playerName : Players.getPlayer(1).playerName;
+            winnerAnnounced = true;
+            updateWinSlots(winner.winnerCombination);
+            sleep(2500).then(() => showResult(playerName));
+            return;
+        }
+        
+        //Draw
+        if(availSpots.length === 0) {
             showResult('Draw!');
             return;
         } 
-        if(winner.winnerSymbol != undefined){
-            const playerName = (winner.winnerSymbol == 'X') ? Players.getPlayer(0).playerName : Players.getPlayer(1).playerName;
-            showResult(playerName);
-            return;
-        }
         changePlayer();
     }
 
+    //change player upon call and call ai_play
     const changePlayer = () => {
         const player1_profile = document.querySelector('.player-1');
         const player2_profile = document.querySelector('.player-2');
@@ -296,14 +315,14 @@ const Game = (() => {
         sleep(1000).then(() => ai_play());
     }
 
+    //if current player is ai make move
     const ai_play = () => {
         const emptySlots = GameBoard.emptySlots();
         // if(GameBoard.getWinner()) return;
         if(emptySlots.length == 0) return;
             const active = (activePlayer == 'X') ? Players.getPlayer(0) : Players.getPlayer(1);
             if(active.ai){
-                const ai_choice = AI.getChoice(1, activePlayer, emptySlots);
-                console.log(ai_choice)
+                const ai_choice = ArtificialIntelligence.getChoice(active.difficulty, activePlayer);
                 const target = document.querySelector(`.box-${ai_choice}`);
                 
                 play({target, type: 'click'});
@@ -311,70 +330,185 @@ const Game = (() => {
             }
     }
     
+    //if current player not ai, make move upon event
     const human_play = (event) => {
         const ai = (activePlayer == 'X') ? Players.getPlayer(0).ai : Players.getPlayer(1).ai;
         if(!ai){
             play(event);
         }
     }
-
+    
+    //make move upon call
     const play = (event) => {
-        if(GameBoard.getWinner()) return;
+        if(winnerAnnounced) return;
+        event.target.removeEventListener(event.type, human_play);
         render(event.target)
         GameBoard.update_gameBoard(event.target.getAttribute('data-id'), activePlayer);
-        event.target.removeEventListener(event.type, play);
         announceWinner();
 
     }
 
+    //setup default value and call ai_play;
     const setup = () => {
         activePlayer = 'X'
+        winnerAnnounced = false;
         ai_play();
     }
 
+    //reset the marks to replay the game
     const replay = (event) => {
         const resultDisplay = document.querySelector('.result .result-model');
         resultDisplay.classList.add('translateY130')
         GameBoard.setup();
         setup();
         event.target.removeEventListener(event.type, replay);
+        setTimeout(() => {
+            document.querySelector('.result').style.zIndex = '-1'
+        }, 300);
     }
+
     return {setup, human_play}
 })();
 
-const AI =(() => {
+
+const ArtificialIntelligence = (() => {
 
     const getRandom = (max=Number, min=0) => {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    const easy = (emptySlots) => {
+    const emptySlots = (board) => {
+        const result = [];
+        for (let i = 0; i < 9; i++){
+            if(!board[i]){
+                result.push(i);
+                }
+        }
+        return result;
+    }
+  
+    const minimax = (player) => {
+  
+    //define constant values
+        const newBoard = GameBoard.get_board;
+        const activePlayer = player;
+        const opponent = (player == 'X') ? 'O' : 'X';
+    
+
+//Help from https://www.freecodecamp.org/news/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37
+        const logic = (newBoard, player) => {
+        
+        //available spots
+        let availSpots = emptySlots(newBoard);
+        
+        // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+        if (GameBoard.checkWinner(activePlayer, newBoard).result){
+        
+            return {score:1};
+        }
+        else if (GameBoard.checkWinner(opponent, newBoard).result){
+
+          return {score:-1};
+        }
+        else if (availSpots.length === 0){
+
+        return {score:0};
+      }
+    
+    // an array to collect all the objects
+      let moves = [];
+    
+      // loop through available spots
+      for (let i = 0; i < availSpots.length; i++){
+        //create an object for each and store the index of that spot that was stored as a number in the object's index key
+        let move = {};
+        move.index = availSpots[i];
+    
+        // set the empty spot to the current player
+        newBoard[availSpots[i]] = player;
+    
+        
+    
+        //if collect the score resulted from calling minimax on the opponent of the current player
+        if (player == activePlayer){
+          let result = logic(newBoard, opponent);
+        //   console.log(result);
+          move.score = result.score;
+        }
+        else{
+          let result = logic(newBoard, activePlayer);
+          move.score = result.score;
+        }
+    
+        //reset the spot to empty
+        delete newBoard[availSpots[i]];
+    
+        // push the object to the array
+        moves.push(move);
+      }
+    // if it is the computer's turn loop over the moves and choose the move with the highest score
+      let bestMove;
+      if(player === activePlayer){
+        let bestScore = -10;
+        for(let i = 0; i < moves.length; i++){
+          if(moves[i].score > bestScore){
+            bestScore = moves[i].score;
+            bestMove = i;
+          }
+        }
+      }else{
+    
+    // else loop over the moves and choose the move with the lowest score
+        let bestScore = 10;
+        for(let i = 0; i < moves.length; i++){
+          if(moves[i].score < bestScore){
+            bestScore = moves[i].score;
+            bestMove = i;
+          }
+        }
+      }
+    
+    // return the chosen move (object) from the array to the higher depth
+    
+      return moves[bestMove];
+    }
+
+    // return the final move (object)
+    return logic(newBoard, player);
+  }
+
+
+    //as name suggest easy choice
+    const easy = () => {
+        const board = GameBoard.get_board;
         return (() => {
             const num = getRandom(8);
-            if(emptySlots.includes(num)){
-                console.log(emptySlots);
+            if(emptySlots(board).includes(num)){
                 return num;
             }else{
-                return easy(emptySlots);
+                return easy();
             }
         })();
     }
-    const medium = () => {
 
+    //same as name suggest hard choice
+    const hard = (activePlayer) => {
+        const ai_move = minimax(activePlayer);
+        return ai_move.index;
     }
-    const hard = (activePlayer, emptySlots) => {
-        
 
-    }
-    const getChoice = (difficulty, activePlayer, emptySlots) => {
+
+    //export Choice
+    const getChoice = (difficulty, activePlayer) => {
 
         switch(difficulty){
-            case 3: return hard();
-            case 2: return medium();
-            case 1: return easy(emptySlots);
+            case '2': return hard(activePlayer);
+            case '1': return easy();
             default: alert('something went wrong');
         }
 
     }
-    return { getChoice }
-})();
+
+  
+  return {getChoice, emptySlots}
+  })()
